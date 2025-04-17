bdf28dd175a732b73457b9cbad0158713e43a277
aoife <amulvenna10@qub.ac.uk>
2025-04-17 12:03:51 +0100

    updated controller for frontend implementation

--- src/main/java/com/example/Spinner/SpinnerApplication.java
+++ src/main/java/com/example/Spinner/SpinnerApplication.java
@@ -1,4 +1,4 @@
-package com.example.Spinner;
+package com.example.spinner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
